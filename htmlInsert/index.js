31d274bb14d6b66b5ed89d4e8411b83d57532fa4
化风 <chh.xin@gmail.com>
2018-06-03 20:03:47 +0800

    test object

---
+++ htmlInsert/index.js
@@ -0,0 +1,7 @@
+var fun = () => {
+    var HTML = '<object type="text/html" data="add.html" width="100%" height="100%"></object>';
+    document.body.innerHTML = HTML;
+}
+window.onload = () => {
+    fun();
+}
